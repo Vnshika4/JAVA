8a7396998a002defc5a3de70af4121d1ca7293a4
Vnshika4 <128033123+Vnshika4@users.noreply.github.com>
2024-04-14 23:51:37 +0530

    exp4f.java

--- exp4f.java
+++ exp4f.java
@@ -1,33 +1,29 @@
-/*Write a program that checks whether a given square matrix is a magic square or not. A magic square is a square matrix where the sum of each row, each column, and both diagonals is the same.*/
-package lp2;
+// Write a program that checks whether a given square matrix is a magic square or not. A magic square is a square matrix where the sum of each row, each column, and both diagonals is the same.
 
 import java.util.Scanner;
 
 public class exp4f {
     public static void main(String[] args) {
-     /*example of magic square matrix int[][] matrix = {
-    	            {2, 7, 6},
-    	            {9, 5, 1},
-    	            {4, 3, 8}
-    	        };*/
-   	 Scanner scanner = new Scanner(System.in);
-	 System.out.println("Enter the size (eg:2x2,3x3) of the matrix:");
-	 int n = scanner.nextInt();
-     int[][] matrix = new int[3][3];
-     System.out.println("Enter the elements of the matrix:");
-     for (int i = 0; i < n; i++) {
-         for (int j = 0; j < n; j++) {
-             matrix[i][j] = scanner.nextInt();
-         }
-     }
-     scanner.close();
-     System.out.println("The entered matrix is:");
-     for (int i = 0; i < n; i++) {
-         for (int j = 0; j < n; j++) {
-             System.out.print(matrix[i][j] + " ");
-         }
-         System.out.println();
-     }
+        System.out.println("Name: Vanshika");
+        System.out.println("Roll No: 2210997266");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size (eg:2x2,3x3) of the matrix:");
+        int n = scanner.nextInt();
+        int[][] matrix = new int[3][3];
+        System.out.println("Enter the elements of the matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        scanner.close();
+        System.out.println("The entered matrix is:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
         System.out.println("Is the given matrix a magic square? " + isMagicSquare(matrix));
     }
 
@@ -61,4 +57,3 @@ public class exp4f {
         return true;
     }
 }
-
