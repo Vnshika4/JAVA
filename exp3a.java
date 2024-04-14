435baeb853012b6845f0c8e49fadb9ebb42b6694
Vnshika4 <128033123+Vnshika4@users.noreply.github.com>
2024-04-14 23:43:19 +0530

    exp3a.java

--- exp3a.java
+++ exp3a.java
@@ -1,20 +1,21 @@
-/*Write a program that takes a student&#39;s score as input and prints out their corresponding
-grade based on the following scale:
-90-100: A
-80-89: B
-70-79: C
-60-69: D
-Below 60: F
-Implement the same with if elseif else and switch-case statements.*/
-package lp2;
+// Write a program that takes a student&#39;s score as input and prints out their corresponding
+// grade based on the following scale:
+// 90-100: A
+// 80-89: B
+// 70-79: C
+// 60-69: D
+// Below 60: F
+// Implement the same with if elseif else and switch-case statements.
 import java.util.Scanner;
 public class exp3a {
     public static void main(String[] args) {
+        System.out.println("Name: Vanshika");
+        System.out.println("Roll No: 2210997266");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the student's score: ");
         int score = scanner.nextInt();
+
         System.out.println("Grade using if elseif else statements : ");
-        // Using if-else statements
         if (score >= 90 && score <= 100) {
             System.out.println("Grade: A");
         } else if (score >= 80 && score < 90) {
@@ -28,8 +29,8 @@ public class exp3a {
         } else {
             System.out.println("Invalid score");
         }
+
         System.out.println("Grade using switch-case statements : ");
-        // Using switch-case statements
         switch (score / 10) {
             case 10:
             case 9:
@@ -47,5 +48,6 @@ public class exp3a {
             default:
                 System.out.println("Grade: F");
         }
+        scanner.close();
     }
 }
