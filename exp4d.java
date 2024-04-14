4acb88a3923d19f60f79361423c3b3f8e8580377
Vnshika4 <128033123+Vnshika4@users.noreply.github.com>
2024-04-14 23:49:45 +0530

    exp4d.java

--- exp4d.java
+++ exp4d.java
@@ -1,26 +1,28 @@
-/*Write a program that calculates the sum of each row and each column in a 2D matrix and displays the results.*/
-package lp2;
+// Write a program that calculates the sum of each row and each column in a 2D matrix and displays the results.
 import java.util.Scanner;
-public class exp4d{
+
+public class exp4d {
     public static void main(String[] args) {
-    	 Scanner scanner = new Scanner(System.in);
-    	 System.out.println("Enter the size (eg:2x2,3x3) of the matrix:");
-    	 int n = scanner.nextInt();
-         int[][] matrix = new int[3][3];
-         System.out.println("Enter the elements of the matrix:");
-         for (int i = 0; i < n; i++) {
-             for (int j = 0; j < n; j++) {
-                 matrix[i][j] = scanner.nextInt();
-             }
-         }
-         scanner.close();
-         System.out.println("The entered matrix is:");
-         for (int i = 0; i < n; i++) {
-             for (int j = 0; j < n; j++) {
-                 System.out.print(matrix[i][j] + " ");
-             }
-             System.out.println();
-         }
+        System.out.println("Name: Vanshika");
+        System.out.println("Roll No: 2210997266");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size (eg:2x2,3x3) of the matrix:");
+        int n = scanner.nextInt();
+        int[][] matrix = new int[3][3];
+        System.out.println("Enter the elements of the matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        scanner.close();
+        System.out.println("The entered matrix is:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
         int size = matrix.length;
         int[] rowSum = new int[size];
         int[] colSum = new int[size];
@@ -34,12 +36,12 @@ public class exp4d{
 
         System.out.println("Sum of each row:");
         for (int i = 0; i < size; i++) {
-            System.out.println("Row " + (i+1) + ": " + rowSum[i]);
+            System.out.println("Row " + (i + 1) + ": " + rowSum[i]);
         }
 
         System.out.println("Sum of each column:");
         for (int i = 0; i < size; i++) {
-            System.out.println("Column " + (i+1) + ": " + colSum[i]);
+            System.out.println("Column " + (i + 1) + ": " + colSum[i]);
         }
     }
 }
