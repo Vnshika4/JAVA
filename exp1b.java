0383d70c8e32d225102e6208a8de15f37654637c
Vnshika4 <128033123+Vnshika4@users.noreply.github.com>
2024-04-14 23:35:56 +0530

    Update exp1b.java

--- exp1b.java
+++ exp1b.java
@@ -1,16 +1,16 @@
-/*Write a Java program to print Pascal's Triangle of n rows.Use nested loops to calculate the values.*/
-package lp2;
+// Write a Java program to print Pascal's Triangle of n rows.Use nested loops to calculate the values.
 public class exp1b {
     public static void main(String[] args) {
-        int n = 5; // number of rows
+        System.out.println("Name: Vanshika");
+        System.out.println("Roll No: 2210997266");
+        int n = 5;
         for(int i = 0; i < n; i++) {
-            int number = 1;
+            int num = 1;
             for(int j = 0; j <= i; j++) {
-                System.out.print(number + " ");
-                number = number * (i - j) / (j + 1);
+                System.out.print(num + " ");
+                num = num * (i - j) / (j + 1);
             }
             System.out.println();
         }
     }
 }
-
